d91bf329d9c13ad9736284e5610d9d9fcfc308ca
corps01 <ocorpus1@outlook.com>
2026-09-24 09:37:05 -0600

    Update WhatsApp contact number to +52 312 242 3596.

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,5 +1,5 @@
-export const WHATSAPP_NUMBER = '523123164000'
-export const WHATSAPP_DISPLAY = '+52 312 316 4000'
+export const WHATSAPP_NUMBER = '523122423596'
+export const WHATSAPP_DISPLAY = '+52 312 242 3596'
 
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(
   'Hola 2Pagi, quiero cotizar mi página web',
